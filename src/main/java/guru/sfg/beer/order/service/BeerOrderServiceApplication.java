745c93cfa97e895685a82dcca64b97c7ee9a5412
sonnyjon <sonny.s.jon@gmail.com>
2022-10-17 06:37:24 -0700

    Minor changes to make MySQL work correctly

--- src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
+++ src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
@@ -2,10 +2,11 @@ package guru.sfg.beer.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
-public class BeerOrderServiceApplication {
-
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+public class BeerOrderServiceApplication
+{
     public static void main(String[] args) {
         SpringApplication.run(BeerOrderServiceApplication.class, args);
     }
